5dad9647166a9c49dc6dd1b78169b3c006e8eb78
Santosh Wadghule <santosh.wadghule@gmail.com>
2018-05-17 23:57:09 +0530

    Added react router

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   scope '/api' do
     resources :drinks
   end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
